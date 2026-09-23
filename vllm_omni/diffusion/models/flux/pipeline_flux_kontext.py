f56866d2066e18bc81c310a1137e5e05aadc42f3
0z5a <dezhen.lu@uni-tuebingen.de>
2026-09-23 16:56:35 +0000

    Fix Kontext import order for pre-commit
    Signed-off-by: 0z5a <dezhen.lu@uni-tuebingen.de>
    Signed-off-by: 0z5a <dezhen.lu@student.uni-tuebingen.de>

--- vllm_omni/diffusion/models/flux/pipeline_flux_kontext.py
+++ vllm_omni/diffusion/models/flux/pipeline_flux_kontext.py
@@ -37,8 +37,8 @@ from vllm_omni.diffusion.models.t5_encoder.quantization import prepare_t5_fp8
 from vllm_omni.diffusion.profiler.diffusion_pipeline_profiler import DiffusionPipelineProfilerMixin
 from vllm_omni.diffusion.utils.tf_utils import get_transformer_config_kwargs
 from vllm_omni.diffusion.worker.request_batch import DiffusionRequestBatch
-from vllm_omni.quantization import resolve_component_quant_config
 from vllm_omni.logger import init_logger
+from vllm_omni.quantization import resolve_component_quant_config
 
 logger = init_logger(__name__)
 
